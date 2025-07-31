8986e5816375646fc2ab8286d68a0e519a2b0994
mutoairi <146906672+mutoairi@users.noreply.github.com>
2025-07-31 10:21:39 +0900

    プロジェクト ファイルを追加します。

---
+++ GameScene.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <KamataEngine.h>
+
+class GameScene {
+public:
+	~GameScene();
+	void Initialize();
+	void Update();
+	void Draw();
+
+private:
+	KamataEngine::DirectXCommon* dxCommon_ = nullptr;
+	KamataEngine::Input* input_ = nullptr;
+	KamataEngine::Audio* audio_ = nullptr;
+
+	
+	
+};
